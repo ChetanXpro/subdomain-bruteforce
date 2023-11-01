e39ea1bc473ce7bd4ca8ef2fb3177e047a343589
chetan <chetan@chetans-MacBook-Air.local>
2023-11-01 22:50:11 +0530

    add cname alias check

--- brute.sh
+++ brute.sh
@@ -5,5 +5,14 @@ while read subdomain; do
    if host "$subdomain.$domain" &> /dev/null; then
          echo "$subdomain.$domain"
    fi
+
+    cname=$(host -t CNAME $subdomain.$domain | grep 'an alias' | awk '{print $NF}')
+
+    if [ -n "$cname" ]; then
+        if ! host $cname &> /dev/null; then
+            echo "$cname did not resolve ($subdomain.$domain)"
+        fi
+    fi
+
 done < subdomains.txt 
     
